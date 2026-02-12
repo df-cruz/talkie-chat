79e3970674990bb6d2d7710835ef286ef71310e5
dfcruz <dfcruz.dev@gmail.com>
2026-02-12 19:19:29 +0000

    Added User foreign key to conversation entity

--- app/src/main/kotlin/com/dfcruz/talkie/data/database/entity/ConversationEntity.kt
+++ app/src/main/kotlin/com/dfcruz/talkie/data/database/entity/ConversationEntity.kt
@@ -1,10 +1,23 @@
 package com.dfcruz.talkie.data.database.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import java.util.Date
 
-@Entity(tableName = "conversations")
+@Entity(
+    tableName = "conversations",
+    foreignKeys = [
+        ForeignKey(
+            entity = UserEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["ownerId"],
+            onDelete = ForeignKey.SET_NULL
+        )
+    ],
+    indices = [Index("ownerId")]
+)
 data class ConversationEntity(
     @PrimaryKey val id: String,
     val ownerId: String? = null,
