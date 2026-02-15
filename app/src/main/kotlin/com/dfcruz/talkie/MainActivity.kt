42dd6bad9dd364267aec9a4a1331d4fdd9c92e2b
dfcruz <dfcruz.dev@gmail.com>
2026-02-15 20:41:28 +0000

    request read contacts permission

--- app/src/main/kotlin/com/dfcruz/talkie/MainActivity.kt
+++ app/src/main/kotlin/com/dfcruz/talkie/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.dfcruz.talkie
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.dfcruz.talkie.ui.navigation.TalkieNavDisplay
 import com.dfcruz.talkie.ui.theme.TalkieTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val permissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        checkAndRequestPermission()
         setContent {
             TalkieTheme {
                 TalkieNavDisplay()
             }
+        }
+    }
+
+    private fun checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionLauncher.launch(Manifest.permission.READ_CONTACTS)
         }
     }
 }
